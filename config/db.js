dd03b4fcaa06b381283e7ca6796298a1f39eab59
imtable <mrelbat@gmail.com>
2022-01-03 13:01:19 +0200

    edit db uri

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  uri: 'mongodb+srv://admin:123123Qq@imtablefreecluster.asr7n.mongodb.net/chat?retryWrites=true&w=majority',
+  uri: 'mongodb+srv://admin:admin@imtablefreecluster.asr7n.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-z4onxb-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
